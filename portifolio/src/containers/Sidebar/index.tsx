78814f4a6b914379977e49f23a34248c43fc2bfd
Karen Aprigio Moraes <karenmoraes222@gmail.com>
2024-06-20 10:12:16 -0300

    desenvolvendo projeto de portifolio

---
+++ portifolio/src/containers/Sidebar/index.tsx
@@ -0,0 +1,27 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
+  <aside>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={12}>Karen Aprigio Moraes</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Xmaloe
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Full Stack
+      </Descricao>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
+    </SidebarContainer>
+  </aside>
+)
+
+export default Sidebar
