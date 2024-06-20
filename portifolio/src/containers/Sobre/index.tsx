17ea79b0666ee85e38383353585f905cd91aa262
Karen Aprigio Moraes <karenmoraes222@gmail.com>
2024-06-20 11:09:49 -0300

    edit error

--- portifolio/src/containers/Sobre/index.tsx
+++ portifolio/src/containers/Sobre/index.tsx
@@ -12,8 +12,8 @@ const Sobre = () => (
       dolor nemo nam?
     </Paragrafo>
     <GithubSecao>
-      <img src="https://github-readme-stats.vercel.app/api?username=Xmaloe&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
-      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Xmaloe&layout=compact&langs_count=7&theme=dracula" />
+      <img src="https://github-readme-stats.vercel.app/api?username=Xmaloe&theme=holi&show_icons=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Xmaloe&layout=compact" />
     </GithubSecao>
   </section>
 )
